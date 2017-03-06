8b1364efd0de258b19eed1df60b26ed3783e8c88
Gurminder Bhachu <gurminderbhachu@hotmail.com>
2017-03-06 13:00:40 +0000

    get and set routes set up and tested

--- lib/app.rb
+++ lib/app.rb
@@ -2,9 +2,19 @@ require 'sinatra/base'
 
 class DBserver < Sinatra::Base
   set :port, 4000
+
   get '/' do
     'Hello Server!'
   end
+
+  get '/set' do
+    'Hello again!'
+  end
+
+  get '/get' do
+    'We really need to stop meeting like this'
+  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/DB_server_spec.rb
+++ spec/features/DB_server_spec.rb
@@ -1,6 +1,19 @@
 feature 'DB Server' do
   scenario 'Can run app and check page content' do
     visit('/')
+    expect(page.status_code).to eq(200)
     expect(page).to have_content 'Hello Server!'
+  end
+
+  scenario 'Can visit the set request' do
+    visit('/set?somekey=1')
+    expect(page.status_code).to eq(200)
+    expect(page).to have_content 'Hello again!'
+  end
+
+  scenario 'Can visit the get request' do
+    visit('/get')
+    expect(page.status_code).to eq(200)
+    expect(page).to have_content 'We really need to stop meeting like this'
   end
 end
